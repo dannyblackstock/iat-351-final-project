14510a54460a1df42235aa55603607236387f8f6
Danny Blackstock <dannyblackstock@gmail.com>
2014-10-20 21:38:12 -0700

    Got a button to send a message with intent to another activity and display it on the screen (following "Build Your First App" tutorial)

--- iat-351-android/IAT351THAWAndroid/src/com/example/iat351thawandroid/MainActivity.java
+++ iat-351-android/IAT351THAWAndroid/src/com/example/iat351thawandroid/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.iat351thawandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	
+	public final static String EXTRA_MESSAGE = "com.example.iat351thawandroid.MESSAGE";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,5 +35,14 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    Intent intent = new Intent(this, DisplayMessageActivity.class);
+	    EditText editText = (EditText) findViewById(R.id.edit_message);
+	    String message = editText.getText().toString();
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
 	}
 }
