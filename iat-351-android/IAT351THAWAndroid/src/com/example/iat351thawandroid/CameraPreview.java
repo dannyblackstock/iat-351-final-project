bc5384ca37b0df68395485db5d3355e3e800af0d
Danny Blackstock <dannyblackstock@gmail.com>
2014-10-28 23:03:20 -0700

    Make the app show a camera preview.

---
+++ iat-351-android/IAT351THAWAndroid/src/com/example/iat351thawandroid/CameraPreview.java
@@ -0,0 +1,82 @@
+
+package com.example.iat351thawandroid;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Build;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private Activity mActivity;
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Activity activity) {
+        super(activity); // Always necessary
+        mActivity = activity;
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (null == mCamera) {
+            mCamera = Camera.open();
+        }
+
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mCamera.stopPreview();
+
+        // Set orientation
+        boolean portrait = isPortrait();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ECLAIR) {
+            // 2.1 and before
+            Parameters mParam = mCamera.getParameters();
+            if (portrait) {
+                mParam.set("orientation", "portrait");
+            } else {
+                mParam.set("orientation", "landscape");
+            }
+            mCamera.setParameters(mParam);
+        } else {
+            // 2.2 and later
+            if (portrait) {
+            	mCamera.setDisplayOrientation(90);
+            } else {
+                mCamera.setDisplayOrientation(0);
+            }
+        }
+
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (null == mCamera) {
+            return;
+        }
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
+
+    protected boolean isPortrait() {
+        return (mActivity.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);
+    }
+}
